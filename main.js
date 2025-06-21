8afa979b3c7c570a05a48b5ce542146d75913349
maar-ten <maar-ten@users.noreply.github.com>
2025-06-21 21:20:01 +0000

    use geo-three

--- main.js
+++ main.js
@@ -1,45 +1,51 @@
-import { Scene, PerspectiveCamera, AmbientLight, Color, LinearSRGBColorSpace, WebGLRenderer } from 'three';
+import { Scene, PerspectiveCamera, AmbientLight, Color, WebGLRenderer } from 'three';
 import { MapControls } from 'three/addons/controls/MapControls.js';
 import { OpenStreetMapsProvider, MapView, UnitsUtils } from 'geo-three';
 
 let camera, controls, scene, renderer;
 
 init();
-//render(); // remove when using animation loop
 
 function init() {
-    const renderer = new WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    renderer = new WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
+    document.body.appendChild(renderer.domElement);
 
-	var scene = new Scene();
-	scene.background = new Color(0.4, 0.4, 0.4, LinearSRGBColorSpace);
-	var provider = new OpenStreetMapsProvider();
-	var map = new MapView(MapView.PLANAR, provider);
-	scene.add(map);
-	map.updateMatrixWorld(true);
-	var camera = new PerspectiveCamera(80, 1, 0.1, 1e12);
-	var controls = new MapControls(camera, renderer.domElement);
-	controls.minDistance = 1e1;
-	controls.zoomSpeed = 2.0;
-	var coords = UnitsUtils.datumsToSpherical(40.940119, -8.535589);
-	controls.target.set(coords.x, 0, -coords.y);
-	camera.position.set(0, 1000, 0);
-	scene.add(new AmbientLight(0x777777));
+    scene = new Scene();
+    scene.background = new Color(0xaad3df); // OSM color of water
+
+    const map = new MapView(MapView.PLANAR, new OpenStreetMapsProvider());
+    map.updateMatrixWorld(true);
+    scene.add(map);
+
+    const coords = UnitsUtils.datumsToSpherical(52.11, 4.77);
+
+    camera = new PerspectiveCamera(80, 1, 0.1, 1e12);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.position.set(coords.x, 100000, -coords.y + 1e-7);
+
+    controls = new MapControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.minDistance = 1e1;
+    controls.zoomSpeed = 2.0;
+    controls.target.set(coords.x, 0, -coords.y);
+
+    scene.add(new AmbientLight(0x777777));
 
     window.addEventListener('resize', onWindowResize);
 }
 
 function onWindowResize() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate() {
     updateFpsCounter();
-    controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+    controls.update();
     render();
 }
 
@@ -54,8 +60,9 @@ function updateFpsCounter() {
     const now = Date.now();
     if (frameCount % 60 === 0) {
         const frameTime = 1000 / (now - lastFrameTime);
-        document.getElementById('fps-counter').innerText = `${Math.round(frameTime)} FPS`;
+        document.getElementById('fps-counter').innerText = `${Math.floor(frameTime)} FPS`;
         frameCount = 0;
+        console.log(camera.position);
     }
     lastFrameTime = now;
     frameCount++;
