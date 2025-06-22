6abfb612598672dce1e80bf7dd6019e7782d8703
maar-ten <maar-ten@users.noreply.github.com>
2025-06-22 09:44:56 +0000

    plot mock aircrafts

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-import { Scene, PerspectiveCamera, AmbientLight, Color, WebGLRenderer } from 'three';
+import { Mesh, MeshBasicMaterial, ConeGeometry, Scene, PerspectiveCamera, HemisphereLight, Color, WebGLRenderer } from 'three';
 import { MapControls } from 'three/addons/controls/MapControls.js';
 import { OpenStreetMapsProvider, MapView, UnitsUtils } from 'geo-three';
 
 let camera, controls, scene, renderer;
 
+const queryString = new URLSearchParams(window.location.search);
+const mapViewLat = Number(queryString.get('lat')) ?? 52.11;
+const mapViewLon = Number(queryString.get('lon')) ?? 4.77;
+
 init();
 
 function init() {
-    const coords = UnitsUtils.datumsToSpherical(52.11, 4.77);
+    renderer = new WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
+    document.body.appendChild(renderer.domElement);
 
     scene = new Scene();
     scene.background = new Color(0xaad3df); // OSM color of water
+
+    const coords = UnitsUtils.datumsToSpherical(mapViewLat, mapViewLon);
 
     camera = new PerspectiveCamera(80, 1, 0.1, 1e12);
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     camera.position.set(coords.x, 100000, -coords.y + 1e-7);
-
-    renderer = new WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setAnimationLoop(animate);
-    document.body.appendChild(renderer.domElement);
 
     const map = new MapView(MapView.PLANAR, new OpenStreetMapsProvider());
     map.updateMatrixWorld(true);
@@ -32,9 +36,44 @@ function init() {
     controls.zoomSpeed = 2.0;
     controls.target.set(coords.x, 0, -coords.y);
 
-    scene.add(new AmbientLight(0x777777));
+    scene.add(new HemisphereLight(0xaad3df, 0xff0000, 4));
+
+    addAircrafts();
 
     window.addEventListener('resize', onWindowResize);
+}
+
+function logError(err) {
+  console.log(`[${err.fileName}:${err.lineNumber}:${err.columnNumber}]`, err.message)
+}
+
+function addAircrafts() {
+    fetch('data.json')
+      .then(response => plotAircrafts(response))
+      .catch(logError);
+}
+
+async function plotAircrafts(response) {
+    if (!response.ok) {
+        throw new Error(`Response not OK: ${response.status}`);
+    }
+
+    const aircraftDataArr = await response.json();
+    aircraftDataArr.forEach(aircraft => scene.add(createAircraft(aircraft)));
+}
+
+function createAircraft({lat, lon, altitude, track}) {
+    const coords = UnitsUtils.datumsToSpherical(lat, lon);
+    const alt = altitude * 0.3048;
+    const heading = track / 360 * 2 * Math.PI;
+
+    const geometry = new ConeGeometry( 5, 10, 3 );
+    const material = new MeshBasicMaterial( { color: 0xff0000 } );
+    const aircraft = new Mesh( geometry, material );
+    aircraft.position.set(coords.x, alt, -coords.y);
+    aircraft.rotation.set(-Math.PI/2, 0, heading);
+    aircraft.scale.set(10, 10, 10);
+    return aircraft;
 }
 
 function onWindowResize() {
@@ -63,7 +102,6 @@ function updateFpsCounter() {
         const frameTime = 1000 / (now - lastFrameTime);
         document.getElementById('fps-counter').innerText = `${Math.floor(frameTime)} FPS`;
         frameCount = 0;
-        console.log(camera.position);
     }
     lastFrameTime = now;
     frameCount++;
